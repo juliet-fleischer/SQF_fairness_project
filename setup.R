78f93347bc85bc501ee6b5e34fc4df588802e92c
juliet-fleischer <juliet@fllb.de>
2024-11-30 14:01:19 +0100

    additional imputation

--- setup.R
+++ setup.R
@@ -11,4 +11,8 @@ library(mice)
 library(checkmate)
 library(iml)
 
-source("program/d
+source("program/data_cleaning.R")
+source("program/functions.R")
+sqf.2023 <- read_excel("data/sqf-2023.xlsx")
+imputed_data <- read_excel("data/imputed_data.xlsx")
+imputed_data[, sapply(imputed_data, is.character)] <- lapply(imputed_data[, sapply(imputed_data, is.character)], as.factor)
