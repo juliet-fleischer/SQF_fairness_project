2a7c559dbcf58d92b3ad55c4307fecdab5521aa6
juliet-fleischer <juliet@fllb.de>
2024-12-07 13:03:29 +0100

    cleaned up the setup script (run at the beginning)

--- setup.R
+++ setup.R
@@ -11,23 +11,13 @@ library(mice)
 library(checkmate)
 library(iml)
 
-
-sqf.2023 <- read_excel("data/sqf-2023.xlsx")
-source("program/data_cleaning.R")
 source("program/functions.R")
+source("program/data_cleaning.R")
+source("program/pre_training.R")
 
-
-
-imputed_data_frisked <- read_excel("data/imputed_data_frisked.xlsx")
-imputed_data_searched <- read_excel("data/imputed_data_searched.xlsx")
-imputed_data_full <- read_excel("data/imputed_data_full.xlsx")
 
 # FIREARM_FLAG until SEARCH_BASIS_OUTLINE_FLAG set all the NA to "N"
+# 
+# imputed_data_arrested$SUSPECT_ARRESTED_FLAG <- as.numeric(imputed_data_arrested$SUSPECT_ARRESTED_FLAG)
+# imputed_data_arrested$FRISKED_FLAG <- as.numeric(imputed_data_arrested$FRISKED_FLAG)
 
-imputed_data_arrested$SUSPECT_ARRESTED_FLAG <- as.numeric(imputed_data_arrested$SUSPECT_ARRESTED_FLAG)
-imputed_data_arrested$FRISKED_FLAG <- as.numeric(imputed_data_arrested$FRISKED_FLAG)
-
-# prop of people arrested AND/ without being searched
-imputed_data_arrested |> 
-  group_by(SUSPECT_ARRESTED_FLAG, FRISKED_FLAG) |> 
-  summarise(count = n() / nrow(imputed_data_arrested))
